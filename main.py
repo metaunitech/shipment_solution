d2610ed7c98d1529d579a605e24f6b4fa9a72a80
metaunitech <metaunitech@163.com>
2024-09-24 15:50:52 +0800

    added;

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class ShipmentFlow:
                 receive_id = message.get('chat_id')
                 receive_type = 'chat_id'
                 mentions = message.get('mentions', [])
-                if not any([i.get('user_id') for i in mentions]):
+                if not any([i.get('id', {}).get('user_id') for i in mentions]):
                     logger.error(f"Not mention current bot. Skipped.{[i.get('user_id') for i in mentions]}")
                     continue
             else:
