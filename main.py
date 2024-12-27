fa7aaeeb4f1894e1a3dff574e390e011368dfba6
Anthony Fan <anthonyf@Anthonys-MacBook-Pro.local>
2024-12-27 23:56:40 +0800

    added;

--- main.py
+++ main.py
@@ -437,7 +437,7 @@ class ShipmentFlow:
             record_results = self.feishu_spreadsheet_handler.batch_get_records(app_token=self.app_token,
                                                                                table_id=table_id,
                                                                                record_ids=records_ids)
-            urls = [i for i in record_results['records']['shared_url']]
+            urls = [i['shared_url'] for i in record_results['records']]
             n_records['目标记录：record ID'] = '\n'.join(records_ids)
             n_records['消息记录'] = '<hr>'.join(urls)
         if document_type:
